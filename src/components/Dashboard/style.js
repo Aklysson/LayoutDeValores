f596ce784cf522a061550acfd3162f493f66ed05
Aklysson Roberto <aklyroberto@hotmail.com>
2022-06-28 04:25:48 -0300

    Adding hover

--- src/components/Dashboard/style.js
+++ src/components/Dashboard/style.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     display: flex;
-    align-items: center;
     justify-content: center;
     margin-top: -5rem;
     gap: 1.5rem;
--- src/components/summary/style.js
+++ src/components/summary/style.js
@@ -9,6 +9,14 @@ gap: 1rem;
 background-color: #ffffff;
 border-radius: 5%;
 padding: 1rem;
+height: 15rem;
+
+:hover{
+    background: linear-gradient(var(--yellow), var(--page-background));
+    cursor: pointer;
+    height:16rem;
+    width: 20rem;
+    margin-top: -1rem;}
 
 h4{
     margin-bottom: 1rem;
